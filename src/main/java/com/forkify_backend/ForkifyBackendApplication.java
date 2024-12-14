7fa7d43d37005b06622ce7dcd00df0bda15eed4f
YBallanger <yann.ballanger@gmail.com>
2024-12-14 10:02:28 +0100

    :tada: feat : init back

---
+++ src/main/java/com/forkify_backend/ForkifyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.forkify_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForkifyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForkifyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forkify_backend/ForkifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forkify_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForkifyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
