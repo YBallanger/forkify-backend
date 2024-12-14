21d284be92e7ed41b2e0ad871a924b3b998e503c
YBallanger <yann.ballanger@gmail.com>
2024-12-14 14:52:00 +0100

    :sparkles: feat : creation entities
    Création entities User, Restaurant et UserVisit

---
+++ src/main/java/com/forkify_backend/entity/Restaurant.java
@@ -0,0 +1,37 @@
+package com.forkify_backend.entity;
+
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "restaurants")
+public class Restaurant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "restaurant_id")
+    private Long restaurantId;
+
+    @NotBlank
+    @Column(name = "restaurant_name", nullable = false, length = 50)
+    private String restaurantName;
+
+    @OneToMany(mappedBy = "restaurant")
+    private Set<UserVisit> restaurants;
+}
---
+++ src/main/java/com/forkify_backend/entity/User.java
@@ -0,0 +1,37 @@
+package com.forkify_backend.entity;
+
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @NotBlank
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    @OneToMany(mappedBy = "user")
+    private Set<UserVisit> userVisits;
+}
